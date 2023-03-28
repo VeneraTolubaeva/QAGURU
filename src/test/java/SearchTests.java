075123f8423434c36a1e96c2350df017d007d2c7
VeneraTolubaeva <venera.to@zohomail.com>
2023-03-28 11:50:05 +0300

    TexstBoxTests + TestBase + CssXpathExamples

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,11 +1,12 @@
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
     @Test
     void successfulSearchTests() {
         open("https://www.google.com/");
-        //$
+        //$("[name=q]").setValue()
     }
 }
---
+++ src/test/java/com/demoqa/TexstBoxTests.java
@@ -0,0 +1,28 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TexstBoxTests extends TestBase {
+
+    @Test
+    void successfulFillFormTest() {
+
+        open("/text-box");
+        //$("id=userName").setValue("");
+        $("#userName").setValue("Venera");
+        $("#userEmail").setValue("aaa@qa.com");
+        $("#currentAddress").setValue("Some street 1");
+        $("#permanentAddress").setValue("Another street 1");
+        $("#submit").click();
+
+        $("#output").shouldHave(text("Venera"), text("aaa@qa.com"),
+                text("Some street 1"), text("Another street 1"));
+
+    }
+}
