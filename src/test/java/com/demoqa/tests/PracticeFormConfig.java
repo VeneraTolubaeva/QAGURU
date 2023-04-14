beaf6e21a6af973f59efd49015ccd4bb701f290d
VeneraTolubaeva <venera.to@zohomail.com>
2023-04-14 13:41:35 +0300

    added PracticeFormTestsWhisComments

--- src/test/java/com/demoqa/tests/PracticeFormConfig.java
+++ src/test/java/com/demoqa/tests/PracticeFormConfig.java
@@ -1,14 +1,15 @@
-package com.demoqa;
+package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
-public class TestBase {
+public class PracticeFormConfig {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1000";
+        Configuration.browserSize = "1920x1080";
         //Configuration.browser = "chrome";
-        //Configuration.timeout = 3000;
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000;
     }
 }
