4e8568959576636e1d268124e19ec89a61a3c5fc
VeneraTolubaeva <venera.to@zohomail.com>
2023-06-12 15:58:11 +0300

    added steps in RemotePracticeFormTest

--- src/test/java/com/demoqa/tests/RemotePracticeFormTest.java
+++ src/test/java/com/demoqa/tests/RemotePracticeFormTest.java
@@ -3,6 +3,7 @@ package com.demoqa.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import io.qameta.allure.Attachment;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.OutputType;
@@ -12,40 +13,51 @@ import static com.codeborne.selenide.Selenide.webdriver;
 import static com.demoqa.pages.RegistrationPage.*;
 import static com.demoqa.testData.RegistrationPageValue.*;
 import static io.qameta.allure.Allure.attachment;
+import static io.qameta.allure.Allure.step;
 
 public class RemotePracticeFormTest extends RemotePracticeFormConfig {
     @Test
     @Tag("remote")
+    @DisplayName("Successful registration remote test")
     void remotePracticeFormTest() {
 
-        registrationPage.openPage()
-                .closeBanner()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setUserEmail(userEmail)
-                .setGenderWrapper(genderValue)
-                .setUserNumber(phoneNumber)
-                .setBirthDate(dayValue, monthsValue, yearValue)
-                .setSubjectsInput(subjectValue)
-                .setHobbies(hobbiesValue)
-                .setUploadPicture(uploadPicture)
-                .setCurrentAddress(currentAddress)
-                .setState(stateValue)
-                .setCity(cityValue)
-                .setSubmit();
+        step("Open form", () -> {
+            registrationPage.openPage()
+                    .closeBanner();
+        });
 
-        registrationPage.verifyRegistrationResultModalAppears()
-                .verifyResult(studentNameField, (firstName+ " " + lastName))
-                .verifyResult(studentEmailField, userEmail)
-                .verifyResult(genderField, genderValue)
-                .verifyResult(mobileField, phoneNumber)
-                .verifyResult(dateBirthField, dayValue+ " " + monthsValue + "," + yearValue)
-                .verifyResult(subjectsField, subjectValue)
-                .verifyResult(hobbiesField, hobbiesValue)
-                .verifyResult(pictureField, uploadPicture)
-                .verifyResult(addressField, currentAddress)
-                .verifyResult(stateCityField, (stateValue + " " + cityValue));
+        step("Fill form", () -> {
+            registrationPage.setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setUserEmail(userEmail)
+                    .setGenderWrapper(genderValue)
+                    .setUserNumber(phoneNumber)
+                    .setBirthDate(dayValue, monthsValue, yearValue)
+                    .setSubjectsInput(subjectValue)
+                    .setHobbies(hobbiesValue)
+                    .setUploadPicture(uploadPicture)
+                    .setCurrentAddress(currentAddress)
+                    .setState(stateValue)
+                    .setCity(cityValue)
+                    .setSubmit();
+        });
 
-        registrationPage.setCloseModal();
+        step("Verify modal", () -> {
+            registrationPage.verifyRegistrationResultModalAppears()
+                    .verifyResult(studentNameField, (firstName + " " + lastName))
+                    .verifyResult(studentEmailField, userEmail)
+                    .verifyResult(genderField, genderValue)
+                    .verifyResult(mobileField, phoneNumber)
+                    .verifyResult(dateBirthField, dayValue + " " + monthsValue + "," + yearValue)
+                    .verifyResult(subjectsField, subjectValue)
+                    .verifyResult(hobbiesField, hobbiesValue)
+                    .verifyResult(pictureField, uploadPicture)
+                    .verifyResult(addressField, currentAddress)
+                    .verifyResult(stateCityField, (stateValue + " " + cityValue));
+        });
+
+        step("Close modal", () -> {
+            registrationPage.setCloseModal();
+        });
     }
 }
