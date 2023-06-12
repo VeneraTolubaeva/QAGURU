88dce4873df59cc3055f40b1f5932af456202711
VeneraTolubaeva <venera.to@zohomail.com>
2023-06-12 15:48:17 +0300

    added config for allure report

--- src/test/java/com/demoqa/tests/RemotePracticeFormConfig.java
+++ src/test/java/com/demoqa/tests/RemotePracticeFormConfig.java
@@ -1,8 +1,12 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demoqa.pages.RegistrationPage;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class RemotePracticeFormConfig {
@@ -15,9 +19,14 @@ public class RemotePracticeFormConfig {
         Configuration.pageLoadStrategy = "eager";
 
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
+    }
+    @BeforeEach
+    void addListener(){
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 }
