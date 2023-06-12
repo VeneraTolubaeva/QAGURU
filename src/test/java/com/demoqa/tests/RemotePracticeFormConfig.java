ce5b69f3fd04a282c4c73013e21096c2c5e6420f
VeneraTolubaeva <venera.to@zohomail.com>
2023-06-12 13:13:12 +0300

    added remote tests

---
+++ src/test/java/com/demoqa/tests/RemotePracticeFormConfig.java
@@ -0,0 +1,17 @@
+package com.demoqa.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.demoqa.pages.RegistrationPage;
+import org.junit.jupiter.api.BeforeAll;
+
+public class RemotePracticeFormConfig {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = "chrome";
+        Configuration.pageLoadStrategy = "eager";
+    }
+}
---
+++ src/test/java/com/demoqa/tests/RemotePracticeFormTest.java
@@ -0,0 +1,57 @@
+package com.demoqa.tests;
+
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import static com.codeborne.selenide.Selenide.webdriver;
+import static com.demoqa.pages.RegistrationPage.*;
+import static com.demoqa.testData.RegistrationPageValue.*;
+import static io.qameta.allure.Allure.attachment;
+
+public class RemotePracticeFormTest extends RemotePracticeFormConfig {
+
+    @Test
+    @Tag("remote")
+    void successfulPracticeFormTest() {
+
+        attachment("Source", webdriver().driver().source());
+
+        registrationPage.openPage()
+                .closeBanner()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setUserEmail(userEmail)
+                .setGenderWrapper(genderValue)
+                .setUserNumber(phoneNumber)
+                .setBirthDate(dayValue, monthsValue, yearValue)
+                .setSubjectsInput(subjectValue)
+                .setHobbies(hobbiesValue)
+                .setUploadPicture(uploadPicture)
+                .setCurrentAddress(currentAddress)
+                .setState(stateValue)
+                .setCity(cityValue)
+                .setSubmit();
+
+        registrationPage.verifyRegistrationResultModalAppears()
+                .verifyResult(studentNameField, (firstName+ " " + lastName))
+                .verifyResult(studentEmailField, userEmail)
+                .verifyResult(genderField, genderValue)
+                .verifyResult(mobileField, phoneNumber)
+                .verifyResult(dateBirthField, dayValue+ " " + monthsValue + "," + yearValue)
+                .verifyResult(subjectsField, subjectValue)
+                .verifyResult(hobbiesField, hobbiesValue)
+                .verifyResult(pictureField, uploadPicture)
+                .verifyResult(addressField, currentAddress)
+                .verifyResult(stateCityField, (stateValue + " " + cityValue));
+
+        registrationPage.setCloseModal();
+    }
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+}
